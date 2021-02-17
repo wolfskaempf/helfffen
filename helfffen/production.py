97cb07687090090b19a104f3d39736d71bffaf87
Tom Wolfskämpf <tom@wolfskaempf.de>
2021-02-17 15:30:00 +0100

    fix database dict

--- helfffen/production.py
+++ helfffen/production.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'helfffen.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
